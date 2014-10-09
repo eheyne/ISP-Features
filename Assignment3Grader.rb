2fe7da2ecfd2ef0140e7e910e6dd0641de533767
Edward Heyne <eheyne@virtualhold.com>
2014-10-08 20:04:18 -0400

    Added Assignment3Grader.rb

---
+++ Assignment3Grader.rb
@@ -0,0 +1,17 @@
+puts "Assignment 3 Grader"
+
+file = ARGV[0]
+
+File.open(file, "r") do |file_handle|
+  file_handle.each_line do |url|
+    sections = url.split("/")
+    id = sections[3] 
+    ENV["STUDENT_URL"] = url.strip
+    echo_student_url_cmd = "echo $STUDENT_URL"
+    value = %x(#{echo_student_url_cmd})
+    puts "STUDENT_URL is #{value}"
+    cucumber_cmd = "cucumber --tag @Ready --format html > ./output/Assignment3/#{id}.html"
+    puts "Executing command: #{cucumber_cmd}"
+    %x(#{cucumber_cmd})
+  end
+end
